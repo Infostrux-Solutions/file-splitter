f674e1657f9d8ac696970684091779e7ebf0b341
Ho Cam <ho@infostrux.com>
2024-03-07 22:13:07 +0000

    Cleaned up files

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from lib.split import Splitter
 
 def run():
 
-    s = Splitter(input_file = 'tests/test_input_vernew.txt')
+    s = Splitter(input_file = 'test/input/sample.txt')
     s.process()
 
 if __name__ == '__main__':
